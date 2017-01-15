9ae32b574315292bd33b1c4334f7e0d9a4b46d8f
Evan Dixon <evanldixon@live.com>
2017-01-15 15:17:54 -0600

    Implemented UI, added readme, and adjusted requirements

--- MagicWordCounter-CodeBehind-Style/MainWindow.xaml.cs
+++ MagicWordCounter-CodeBehind-Style/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,96 @@ namespace MagicWordCounter_CodeBehind_Style
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public string TextToCount {
+            get
+            {
+                return txtToCount.Text;
+            }
+            set
+            {
+                txtToCount.Text = value;
+            }
+        }
+
+        public bool ExcludeArticles
+        {
+            get
+            {
+                return chbExcludeArticles.IsChecked.Value;
+            }
+            set
+            {
+                chbExcludeArticles.IsChecked = value;
+            }
+        }
+
+        public bool CountQuotesAsOneWord
+        {
+            get
+            {
+                return chbQuotes.IsChecked.Value;
+            }
+            set
+            {
+                chbQuotes.IsChecked = value;
+            }
+        }
+
+        public string LabelText
+        {
+            get
+            {
+                return (string)lblCount.Content;
+            }
+        }
+
+        private bool isArticle(string word)
+        {
+            return word.ToLower() == "a" || word.ToLower() == "an" || word.ToLower() == "the";
+        }
+
+        /// <summary>
+        /// Regular expression that matches text inside quotation marks (including the quotation marks), taking into account smart quotes.
+        /// </summary>
+        private static Regex QuoteRegex => new Regex($"(\\\"|\\{(char)0x201C}|\\{(char)0x201D})((.|\n)*?)(\\\"|\\{(char)0x201C}|\\{(char)0x201D})", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public void btnCount_Click(object sender, RoutedEventArgs e)
+        {
+            var textToCount = txtToCount.Text.Replace(Environment.NewLine, " ").Replace("…", " ");
+            var wordCount = 0;
+
+            // Count words, taking into account article adjective settings
+            foreach (var word in textToCount.Split(' '))
+            {
+                if (!string.IsNullOrEmpty(word) && (!isArticle(word) || !chbExcludeArticles.IsChecked.Value))
+                {
+                    wordCount += 1;
+                }
+            }
+
+            // Take into account quotations
+            if (chbQuotes.IsChecked.Value)
+            {
+                foreach (Match match in QuoteRegex.Matches(textToCount))
+                {
+                    // Count the words inside the quotation marks
+                    var quoteCount = 0;
+                    foreach (var word in match.Groups[2].Value.Split(' '))
+                    {
+                        if (!string.IsNullOrEmpty(word) && (!isArticle(word) || !chbExcludeArticles.IsChecked.Value))
+                        {
+                            quoteCount += 1;
+                        }
+                    }
+
+                    // Because the words inside the quote have already been counted in wordCount, subtract the number of words in the quote from wordCount
+                    wordCount -= (quoteCount - 1); //The "- 1" part makes it so that quotes count as 1 word, instead of 0
+                }
+            }
+
+            lblCount.Content = $"{wordCount} word(s)";
         }
     }
 }
--- MagicWordCounter.Tests/CountingTests.cs
+++ MagicWordCounter.Tests/CountingTests.cs
@@ -1,27 +1,56 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MagicWordCounter_CodeBehind_Style;
 
 namespace MagicWordCounter.Tests
 {
     [TestClass]
     public class CountingTests
     {
+
+        MainWindow testWindow;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            testWindow = new MainWindow();
+            testWindow.TextToCount = Properties.Resources.TestString;
+        }
+        
         [TestMethod]
         public void BasicCounting()
         {
-            throw new NotImplementedException();
+            testWindow.ExcludeArticles = false;
+            testWindow.CountQuotesAsOneWord = false;
+            testWindow.btnCount_Click(null, null);
+            Assert.AreEqual("34 word(s)", testWindow.LabelText);
         }
 
         [TestMethod]
         public void ExcludeArticles()
         {
-            throw new NotImplementedException();
+            testWindow.ExcludeArticles = true;
+            testWindow.CountQuotesAsOneWord = false;
+            testWindow.btnCount_Click(null, null);
+            Assert.AreEqual("32 word(s)", testWindow.LabelText);
         }
 
         [TestMethod]
-        public void NamesAreOneWord()
+        public void QuotesAreOneWord()
         {
-            throw new NotImplementedException();
+            testWindow.ExcludeArticles = false;
+            testWindow.CountQuotesAsOneWord = true;
+            testWindow.btnCount_Click(null, null);
+            Assert.AreEqual("29 word(s)", testWindow.LabelText);
+        }
+
+        [TestMethod]
+        public void ExcludeArticlesAndCountQuotesAsOneWord()
+        {
+            testWindow.ExcludeArticles = true;
+            testWindow.CountQuotesAsOneWord = true;
+            testWindow.btnCount_Click(null, null);
+            Assert.AreEqual("28 word(s)", testWindow.LabelText);
         }
     }
 }
